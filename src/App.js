396df649fd7308ff10e7319056d9db76f4aaff24
Hankchiefz <167849416+Hankchiefz@users.noreply.github.com>
2024-06-21 14:51:06 +1000

    Add files via upload
    Added navbar, for the home, about and contact, made the  register for students and teachers, and the login page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './Components/objects/Navbar';
+import About from './Components/pages/About';
+import Contact from './Components/pages/Contact';
+import Home from './Components/pages/Home';
+import Signup from './Components/pages/Signup';
+import Tsign from './Components/pages/Tsignup';
+import Login from './Components/pages/Login';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/teacher-signup" element={<Tsign />} />
+                <Route path="/login" element={<Login />} /> 
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
