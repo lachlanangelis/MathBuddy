958aebe8e721266993694edd3acc22f40860d3f9
Hankchiefz <167849416+Hankchiefz@users.noreply.github.com>
2024-07-09 20:15:28 +1000

    Updated routing efficiency

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// src/App.js
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import NavBar from './Components/objects/Navbar';
 import About from './Components/homepages/About';
 import Contact from './Components/homepages/Contact';
@@ -6,21 +7,38 @@ import Home from './Components/homepages/Home';
 import Signup from './Components/homepages/Signup';
 import Tsign from './Components/homepages/Tsignup';
 import Login from './Components/homepages/Login';
+import StudentHome from './Components/studentpages/StudentHome';
+import StudentQuiz from './Components/studentpages/StudentQuiz';
 
 function App() {
-    return (
-        <Router>
-            <NavBar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/teacher-signup" element={<Tsign />} />
-                <Route path="/login" element={<Login />} /> 
-            </Routes>
-        </Router>
-    );
+  const location = useLocation();
+
+  // List of paths where NavBar from home should be hidden
+  const hideNavBarPaths = ['/studenthome', '/studentquiz'];
+
+  return (
+    <>
+      {!hideNavBarPaths.includes(location.pathname) && <NavBar />} {/* Conditionally render NavBar */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/teacher-signup" element={<Tsign />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/studenthome" element={<StudentHome />} />
+        <Route path="/studentquiz" element={<StudentQuiz />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App;
+function AppWrapper() {
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
+}
+
+export default AppWrapper;
