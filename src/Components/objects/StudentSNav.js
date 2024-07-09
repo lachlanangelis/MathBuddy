d7a5edcd94d2594ef9725f1f1f29d16dcec29ec4
Hankchiefz <167849416+Hankchiefz@users.noreply.github.com>
2024-07-09 20:18:05 +1000

    Updated SNavbar added objects for quiz

--- src/Components/objects/StudentSNav.js
+++ src/Components/objects/StudentSNav.js
@@ -1,21 +1,28 @@
 // src/Components/objects/StudentSNav.js
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './StudentSNav.css';
 
 const StudentSNav = () => {
   return (
     <div className="sidebar">
-      <div className="menu-icon">&#9776;</div> {/* Menu icon */}
-      <div className="nav-item">
-        <span className="nav-text">Dashboard</span>
-      </div>
-      <div className="nav-item">
+      <Link to="/studenthome" className="menu-link">
+        <div className="menu-icon">&#9776;</div> {/* Menu icon */}
+      </Link>
+      <Link to="/studenthome" className="nav-link">
+        <div className="nav-item">
+          <span className="nav-text">Dashboard</span>
+        </div>
+      </Link>
+      <div className="nav-item classes">
         <span className="nav-icon">&#128203;</span> {/* Classes icon */}
         <span className="nav-text">Classes</span>
       </div>
       <div className="nav-item">
-        <span className="nav-icon">&#128467;</span> {/* Quizzes icon */}
-        <span className="nav-text">Quizzes</span>
+        <Link to="/studentquiz" className="nav-link">
+          <span className="nav-icon">&#128467;</span> {/* Quizzes icon */}
+          <span className="nav-text">Quizzes</span>
+        </Link>
       </div>
       <div className="nav-item">
         <span className="nav-icon">&#128214;</span> {/* Lessons icon */}
