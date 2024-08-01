c2f430788c2aa17b48c71dfe0b0830929bd26d24
Hankchiefz <167849416+Hankchiefz@users.noreply.github.com>
2024-08-01 20:19:20 +1000

    More routes for the teacher and student side

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// src/App.js
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import NavBar from './Components/objects/Navbar';
 import About from './Components/homepages/About';
@@ -9,12 +8,23 @@ import Tsign from './Components/homepages/Tsignup';
 import Login from './Components/homepages/Login';
 import StudentHome from './Components/studentpages/StudentHome';
 import StudentQuiz from './Components/studentpages/StudentQuiz';
-
+import StudentLessons from './Components/studentpages/StudentLessons';
+import StudentClasses from './Components/studentpages/StudentClasses';
+import StudentQuizI from './Components/studentinner/StudentQuizI';
+import TActiveTasks from './Components/teacherpages/TActiveTasks'; 
+import TeacherHomepage from './Components/teacherpages/TeacherHomepage'; 
+import StudentFeedback from './Components/studentpages/StudentFeedback';
+import Tclasses from './Components/teacherpages/Tclasses';
+import TNewClass from './Components/teacherinner/TNewClass';
+import StudentQuizComplete from './Components/studentinner/StudentQuizComplete';
+import TNewQuiz from './Components/teacherinner/TNewQuiz';
 function App() {
   const location = useLocation();
 
   // List of paths where NavBar from home should be hidden
-  const hideNavBarPaths = ['/studenthome', '/studentquiz'];
+  const hideNavBarPaths = ['/studenthome', '/studentquiz', '/studentlessons', '/studentclasses', 
+  '/studentquizi', '/TActiveTasks', '/studentfeedback', '/teacherhomepage', '/Tclasses', '/tnewclass', 
+  '/studentquizcomplete', '/tnewquiz'];
 
   return (
     <>
@@ -28,6 +38,16 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/studenthome" element={<StudentHome />} />
         <Route path="/studentquiz" element={<StudentQuiz />} />
+        <Route path="/studentlessons" element={<StudentLessons />} /> 
+        <Route path="/studentclasses" element={<StudentClasses />} /> 
+        <Route path ="/studentfeedback" element={<StudentFeedback/>} />
+        <Route path="/studentquizi" element={<StudentQuizI />} /> 
+        <Route path="/tactivetasks" element={<TActiveTasks />} /> 
+        <Route path = "/teacherhomepage" element={<TeacherHomepage />} />
+        <Route path = "/tclasses" element={<Tclasses />} />
+        <Route path = "/tnewclass" element={<TNewClass/>} />
+        <Route path ='/studentquizcomplete' element = {<StudentQuizComplete/>} />
+        <Route path = '/tnewquiz' element= {<TNewQuiz />} />
       </Routes>
     </>
   );
